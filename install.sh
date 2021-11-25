b14745e51ef1090feecb0a53af1aaa3e5b6f1bef
Dograbbit <p81061473525@gmail.com>
2021-11-25 17:56:13 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # design by cheng
+# 
 # docker-compose -f docker-compose_v3_centos_mysql_latest.yaml kill 
-# docker-compose --profile debug up
+# docker-compose -f docker-compose_v3_centos_mysql_latest.yaml --profile all up -d
+# 
